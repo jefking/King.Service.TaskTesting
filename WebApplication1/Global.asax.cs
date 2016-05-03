45a224c45969fa9e7977fc087ea59b0488938e2f
Jef King <jefkin@microsoft.com>
2016-05-03 08:42:10 -0700

    testing

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using King.Service;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private IRoleTaskManager<object> manager = new RoleTaskManager<object>(new Factory());
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            this.manager.Run();
+            this.manager.OnStart();
+        }
+    }
+}
---
+++ WebApplication1/KingService.cs
@@ -0,0 +1,48 @@
+﻿using King.Azure.Data;
+using King.Service;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Factory : ITaskFactory<object>
+    {
+        public IEnumerable<IRunnable> Tasks(object passthrough)
+        {
+            yield return new RecurringRunner(new Runs(new BackgroundBackground()));
+        }
+    }
+
+    public class Runs : IRuns
+    {
+        IProcessor<object> task;
+        public Runs(IProcessor<object> task)
+        {
+            this.task = task;
+        }
+        public int MinimumPeriodInSeconds
+        {
+            get
+            {
+                return 5;
+            }
+        }
+
+        public async Task<bool> Run()
+        {
+            return await task.Process(null);
+        }
+    }
+
+
+    public class BackgroundBackground : IProcessor<object>
+    {
+        public async Task<bool> Process(object data)
+        {
+            await Task.Run(() => { Thread.Sleep(5000); });
+
+            return true;
+        }
+    }
+}
